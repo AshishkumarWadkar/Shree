f7f418e8f42cfc6b468e6be18611b8a5855315c5
AshishkumarWadkar <wadkarashish1@gmail.com>
2022-11-02 07:24:58 +0530

    Boiler Add done by Ashish

---
+++ app/Models/BoilerMaster.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BoilerMaster extends Model
+{
+    use HasFactory;
+    protected $table = "bolier_master";
+}
